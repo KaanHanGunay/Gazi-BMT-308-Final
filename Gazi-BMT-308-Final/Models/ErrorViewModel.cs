0a8f683737fd5c062df5aa0ebe1ca593f9711882
kaanhangunay <kaangunay16@gmail.com>
2023-05-30 20:41:57 +0300

    Initial commit

---
+++ Gazi-BMT-308-Final/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Gazi_BMT_308_Final.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
